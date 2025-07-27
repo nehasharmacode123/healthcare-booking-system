4f5f11c1939f82e7556c4f9a6e3656d89b331253
nehasharmacode123 <nehasharma962595@gmail.com>
2025-07-27 22:56:32 +0530

    first commit

---
+++ myNotes.md
@@ -0,0 +1,55 @@
++++++++++++++++++++++++ PROJECT SETUP ++++++++++++++++++++++++++++
+
+1.) npm create vite@latest 
+cd frontend
+npm i
+
+2.) npm install axios react-router-dom react-toastify
+
+3.) npm run dev
+
+4.) Add tailwind :-
+    Perfect — here’s a full setup of a React + Vite project using Tailwind CSS v4 (with @import "tailwindcss/preflight").
+
+    project-root/
+├── index.html
+├── package.json
+├── postcss.config.js
+├── tailwind.config.js
+├── vite.config.js
+└── src/
+    ├── index.css ✅
+    ├── main.jsx
+    ├── App.jsx
+    └── components/
+        └── Navbar.jsx
+
+
+
+5.) add assets in assets folder
+
+6.) create src in components folder
+
+7.) create src in pages folder
+
+8.) go to main.jsx file 
+    import {BrowserRouter} from 'react-router-dom'
+     <BrowserRouter>
+    <App />
+    </BrowserRouter>
+
+9.) create another folder in src in context folder
+
+10.) pages folder in create multiple file 
+    Home.jsx
+    Doctors.jsx
+    Contact.jsx
+    About.jsx
+    Login.jsx
+    MyProfile.jsx
+    MyAppointments.jsx
+
+11.) set the our routes
+    open the App.jsx file
+    
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col md:flex-row flex-wrap bg-blue-500 rounded-lg px-6 md:px-10 lg:px-20'>
+
+        {/* ----------- Left Side ----------  */}
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py[-10vw] md:mb-[-30px]'>
+            <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+            Book Appointment <br /> With Trusted Doctors 
+            </p>
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-28' src={assets.group_profiles} alt="" />
+                <p>Simply browse through our extensive list of trusted doctors, <br /> schedule your appointment hassle-free.</p>
+            </div>
+            <a href="">
+                Book appointment <img src={assets.arrow_icon} alt="" />
+            </a>
+        </div>
+
+        {/* ----------- Right Side ----------  */}
+        <div className='md:w-1/2 relative '>
+            <img className='w-full' src={assets.header_img} alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
